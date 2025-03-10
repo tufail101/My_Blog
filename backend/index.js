09bf074ee0c530facb564396e8c87ac6f4dfe38e
Tufail Ansari <thebinaryblasters@gmail.com>
2025-03-11 00:24:50 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors({
   origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  // allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 app.get("/", (req, res) => {
